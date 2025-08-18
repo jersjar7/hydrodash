30aade2242235bf26751eb263675f5edc3ef3126
Jerson <jerson.7@icloud.com>
2025-08-18 17:17:59 -0600

    sidebar toggle button done

--- components/Layout/MapPanel.tsx
+++ components/Layout/MapPanel.tsx
@@ -76,7 +76,7 @@ const MapPanel: React.FC<MapPanelProps> = ({
 
       {/* Map Controls Overlay */}
       {controls && (
-        <div className="absolute top-4 right-10 z-20 pointer-events-none">
+        <div className="absolute top-6 right-10 z-20 pointer-events-none">
           <div className="pointer-events-auto">
             {controls}
           </div>
